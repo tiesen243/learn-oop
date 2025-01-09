63b70f53e4bb52274aa6cb703ffa3f0967f1d3f5
tiesen243 <ttien56906@gmail.com>
2025-01-09 18:59:18 +0700

    feat(student): add student class

--- example/src/App.java
+++ example/src/App.java
@@ -1,26 +1,12 @@
-import java.util.Scanner;
-
 public class App {
-    static void InTGPascal(int n) {
-        int[][] arr = new int[n][n];
+  public static void main(String[] args) {
+    Student a = new Student(1, "Alice", 20);
+    Student b = new Student(2, "Bob", 21);
+    Student c = new Student(3, "Charlie", 22);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (i == j || j == 0)
-                    arr[i][j] = 1;
-                else
-                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of x: ");
-        double x = sc.nextDouble();
-        System.out.println("Sigmoid of " + x + " is: " + utils.sigmoid(x));
-        sc.close();
-    }
+    System.out.println("ID\tName\tAge");
+    System.out.println(a.getId() + "\t" + a.getName() + "\t" + a.getAge());
+    System.out.println(b.getId() + "\t" + b.getName() + "\t" + b.getAge());
+    System.out.println(c.getId() + "\t" + c.getName() + "\t" + c.getAge());
+  }
 }
--- example/src/utils.java
+++
@@ -1,9 +0,0 @@
-public class utils {
-    public static double sigmoid(double x) {
-        return 1 / (1 + Math.exp(-x));
-    }
-
-    public static double tanh(double x) {
-        return Math.tanh(x);
-    }
-}
