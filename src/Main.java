8fbbaafc4e3a55cac23a2c189bf5f5d9a90b7f6d
tiesen243 <ttien56906@gmail.com>
2025-02-27 23:32:15 +0700

    chore: hello

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,6 @@
 public class Main {
 
   public static void main(String[] args) {
-    A a1 = new A(2, 3);
-    a1.show();
-
-    A a2 = new A();
-    a2.show();
-  }
-}
-
-class A {
-
-  private Integer x;
-  private Integer y;
-
-  private A(Integer x, Integer y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  public A() {
-    this(6, 8);
-  }
-
-  public void show() {
-    System.out.println(x + y);
+    System.out.println("hello");
   }
 }
